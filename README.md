d827f94131ea31b67a09cd980b7e24106241362e
Soumic <fahim.farhan@outlook.com>
2022-07-21 01:31:40 +0600

    :bookmark: `Feat(create-project):` Creates project

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # JavaSpringTutorial
+
+## Class 4
+1. Go to start.spring.io, select language, version etc.
+2. Add dependencies. The initial dependency is `Spring web`
---
+++ applications/src/test/java/indiedev/soumic/applications/ApplicationsApplicationTests.java
@@ -0,0 +1,13 @@
+package indiedev.soumic.applications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
